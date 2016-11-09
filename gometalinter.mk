5a449ca941f62921cb05f480e96d5018ab315ea8
Matt Vinall <matt.vinall@imgtec.com>
2016-11-09 12:10:38 +0000

    gometalinter: don't specify all linters by default - some projects are re-eeeally slow otherwise - makefiles that call this one can add --fast or --enable-all if they want

--- gometalinter.mk
+++ gometalinter.mk
@@ -21,7 +21,6 @@ GOMETALINTER_OPT:=\
 	--exclude='should have comment or be unexported' \
 	--exclude='should have comment \(or a comment on this block\) or be unexported' \
 	--exclude='error return value not checked \(defer ' \
-	--enable-all \
 	--deadline=$(GOMETALINTER_DEADLINE) \
 	--line-length=$(GOMETALINTER_LINELENGTH) \
 	--cyclo-over=$(GOMETALINTER_CYCLO)
