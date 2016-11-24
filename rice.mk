956896ece3127f01649e7e5677b34f3846acbfcb
Matt Vinall <Matt.Vinall@imgtec.com>
2016-11-24 09:52:27 +0000

    rice: use "appendbox" git branch - hoping that this will get upstreamed sometime soon

--- rice.mk
+++ rice.mk
@@ -2,4 +2,5 @@ export RICE:=$(GOPATH)/bin/rice
 
 $(RICE): | $(GOPATH)
 	$(call PROMPT,Installing $@)
-	$(GO) get github.com/boyvinall/go.rice/...
+	git clone -b appendbox https://github.com/boyvinall/go.rice.git $(GOPATH)/src/github.com/GeertJohan/go.rice
+	$(GO) install github.com/GeertJohan/go.rice/...
