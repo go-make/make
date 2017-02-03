5df4cd143102f1872d4dceca871831d24eaf219e
Matt Vinall <Matt.Vinall@imgtec.com>
2017-02-03 16:40:28 +0000

    lint: handle some protobuf warnings with go 1.8

--- gometalinter.mk
+++ gometalinter.mk
@@ -19,12 +19,19 @@ GOMETALINTER_CYCLO:=15
 endif
 
 ifndef GOMETALINTER_OPT
+
+# protobuf-generated code fails "go lint" with a couple of things...
+GOMETALINTER_OPT_PROTOBUF:=\
+	--exclude="don't use underscores in Go names; (func _[A-Za-z]+_[A-Za-z]+_Handler|var _[A-Za-z]+_serviceDesc) should be" \
+	--exclude="context.Context should be the first parameter of a function"
+
 GOMETALINTER_OPT:=\
 	--exclude='should have comment or be unexported' \
 	--exclude='should have comment \(or a comment on this block\) or be unexported' \
 	--exclude='error return value not checked \(defer ' \
-	--exclude="don't use underscores in Go names; (func _[A-Za-z]+_[A-Za-z]+_Handler|var _[A-Za-z]+_serviceDesc) should be" \
+	$(GOMETALINTER_OPT_PROTOBUF) \
 	--deadline=$(GOMETALINTER_DEADLINE) \
 	--line-length=$(GOMETALINTER_LINELENGTH) \
 	--cyclo-over=$(GOMETALINTER_CYCLO)
+
 endif
