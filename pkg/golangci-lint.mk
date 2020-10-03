edfd321b2fbadbb17f961840e3c926643368610c
Aram <aram.petrosyan.88@gmail.com>
2020-10-03 21:49:41 +0100

    Remove dep and gometalinter (#2)
    Co-authored-by: Aram <aram.petrosyan@unity3d.com>

--- pkg/golangci-lint.mk
+++ pkg/golangci-lint.mk
@@ -2,7 +2,7 @@ export GOLANGCI_LINT:=$(GOPATH)/bin/golangci-lint
 export _SELF:=$(lastword $(MAKEFILE_LIST))
 GOLANGCI_LINT_VERSION:=v1.23.8
 
-# grab the gometalinter binary and install the actual linters
+# grab the golangci binary and install the actual linters
 $(GOLANGCI_LINT): | $(GOPATH)
 	$(call PROMPT,Installing $@)
 	curl -sfL https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | sh -s -- -b $(GOPATH)/bin $(GOLANGCI_LINT_VERSION)
